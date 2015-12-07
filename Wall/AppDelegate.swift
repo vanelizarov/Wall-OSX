9a780b1842fb77170cf868b1c3423414af9a502c
vanelizarov <elizarov.vanya@gmail.com>
2015-12-07 21:48:57 +0300

    Initial Commit

---
+++ Wall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wall
+//
+//  Created by vanelizarov on 07/12/15.
+//  Copyright © 2015 vanelizarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Wall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wall
+//
+//  Created by vanelizarov on 07/12/15.
+//  Copyright © 2015 vanelizarov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
